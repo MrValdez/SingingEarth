90fba122efc9e946b33bc0a9eea439508cd048e0
MrValdez <shunyvaldez@gmail.com>
2016-04-23 06:07:39 +0800

    added ISS coordinates

---
+++ ISS.py
@@ -0,0 +1,20 @@
+# http://open-notify.org/Open-Notify-API/ISS-Location-Now/
+
+import json
+try:
+    import urllib2 as urllib
+except ImportError:
+    import urllib.request as urllib
+
+
+def coord():
+    req = urllib.Request("http://api.open-notify.org/iss-now.json")
+    response = urllib.urlopen(req)
+    data = response.read()
+
+    obj = json.loads(data.decode("utf-8"))
+
+    latitude, longitude = (obj['iss_position']['latitude'],
+                           obj['iss_position']['latitude'])
+
+    return latitude, longitude
