599c55e988c5efea300d3f7034d65046c650a26a
MrValdez <shunyvaldez@gmail.com>
2016-04-23 14:01:39 +0800

    added youtube searching

--- song.py
+++ song.py
@@ -1,9 +1,11 @@
+import re
+import urllib
 from collections import namedtuple
 
 import billboard
 
-song_info = named_tuple('song-info',
-                        'name artist spotify')
+song_info = namedtuple('song_info',
+                       'name artist spotify url')
 
 billboard_countries = [("us", "hot-100"),
                        ("japan", "japan-hot-100"),
@@ -29,7 +31,8 @@ def _grab_billboard(country):
     songs = [song[0] for song in songs]
     songs = [song_info(name=song.title,
                        artist=song.artist,
-                       spotify=song.spotify)
+                       spotify=song.spotifyLink,
+                       url=_find_url(song.title))
              for song in songs]
 
     return songs
@@ -46,9 +49,23 @@ def _grab_data(country):
     return None
 
 
+def _find_url(title):
+    '''
+    return the youtube url of song
+    '''
+    # http://www.codeproject.com/Articles/873060/Python-Search-Youtube-for-Video
+    query_string = urllib.parse.urlencode({"search_query": title})
+    url = "http://www.youtube.com/results?" + query_string
+    html_content = urllib.request.urlopen(url)
+    search_results = re.findall(r'href=\"\/watch\?v=(.{11})',
+                                html_content.read().decode())
+    url = ["http://www.youtube.com/watch?v=" + id for id in search_results]
+    return url[0]
+
+
 def grab(country):
     '''
         returns one song and the player
     '''
     songs = _grab_data(country)
-    return song[0]
+    return songs[0]
